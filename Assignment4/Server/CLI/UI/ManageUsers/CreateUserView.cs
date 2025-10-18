5164cf383010c29bfc047acaf1cf1593ba5aba06
Mojkaw <hiaygun@gmail.com>
2025-10-18 16:16:47 +0200

    [Add] Assignment4 WebAPI
    Contracts, Controllers

---
+++ Assignment4/Server/CLI/UI/ManageUsers/CreateUserView.cs
@@ -0,0 +1,37 @@
+﻿using RepositoryContracts;
+using Entities;
+
+namespace CLI.UI.ManageUsers;
+
+public sealed class CreateUserView
+{
+    private readonly IUserRepository userRepository;
+    
+    public CreateUserView(IUserRepository userRepository)
+        {
+        this.userRepository = userRepository;
+        }
+
+    public async Task ShowAsync()
+    {
+        string username;
+        do
+        {
+            Console.WriteLine("Please enter a username:");
+            username = Console.ReadLine()?.Trim() ?? "";
+            if (string.IsNullOrEmpty(username)) Console.WriteLine("Username is required.");
+        } while (string.IsNullOrEmpty(username));
+        
+        string password;
+        do
+        {
+            Console.WriteLine("Please enter a password:");
+            password = Console.ReadLine()?.Trim() ?? "";
+            if (string.IsNullOrEmpty(password)) Console.WriteLine("Password is required.");
+        } while (string.IsNullOrEmpty(password));
+        
+        var user = new User {Username = username, Password = password};
+        await userRepository.AddAsync(user);
+        Console.WriteLine("User created.");
+    }
+}
---
+++ Assignment4/Shared/ApiContracts/CommentsDtos.cs
@@ -0,0 +1,21 @@
+﻿namespace ApiContracts;
+
+public class CreateCommentDto
+{
+    public int UserId { get; set; }
+    public int PostId { get; set; }
+    public required string Body { get; set; }
+}
+
+public class UpdateCommentDto
+{
+    public required string Body { get; set; }
+}
+
+public class CommentDto
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int PostId { get; set; }
+    public required string Body { get; set; }
+}
